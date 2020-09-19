0c4304d459865c933554ca1cb7e1f828da9fb5b5
David <davidbrown3142@gmail.com>
2020-09-19 10:53:30 -0400

    getTask

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -5,6 +5,8 @@ init();
 function init() {
     generateTimeblocks();
     generateTasks();
+    $(document).on("click", ".timeblock", () => placeTaskState.state ? placeTask() : startGetTask() );
+    $(document).on("click", ".task", () => getTaskState.state ? getTask() : startPlaceTask() );
 }
 
 function startPlaceTask() {
@@ -22,3 +24,22 @@ function placeTask() {
 }
 
 function storeInfo() {localStorage.setItem("task-planner-data", JSON.stringify(info) )}
+
+// When you click on a timeblock to place a task in it
+function startGetTask() {
+    info.getTaskState = {
+        state: true,
+        // taskArea is not fully formed yet, currently it's "#id"
+        taskArea: `#${$(this).attr("id")}-task`
+    };
+    storeInfo();
+    changePage("tasks");
+}
+
+// When you click a task to place it in a timeblock
+function getTask() {
+    $(info.getTaskState.taskArea).text($(this).attr("data-task"));
+    info.getTaskState.state = false;
+    storeInfo();
+    changePage("schedule");
+}
