6dcd0a5f8e8bc573a0dfd93aa2cc982a633d5ab1
David <davidbrown3142@gmail.com>
2020-09-19 10:47:27 -0400

    placeTask

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -6,3 +6,17 @@ function init() {
     generateTimeblocks();
     generateTasks();
 }
+
+function startPlaceTask() {
+    var taskData = $(this).attr("data-task");
+    changePage("schedule");
+    info.placeTaskState = {
+        state: true,
+        task: taskData,
+    };
+    storeInfo();
+}
+
+function placeTask() {
+    $(`#${$(this).attr("data-timeblock")}-task`).text(info.placeTaskState.task);
+}
