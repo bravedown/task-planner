5cf0f6a0e0227192baeaf2604d43753a40b0efea
David <davidbrown3142@gmail.com>
2020-09-19 11:03:13 -0400

    fixing changePage

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -66,7 +66,44 @@ function makeTimeblock(hour){
 }
 
 function changePage(goTo){
-    $("#tasklist").display = none;
-    $("#schedule").display = none;
-    $(goTo).display = block;
+    $("#tasklist").css("display", "none");
+    $("#schedule").css("display", "none");
+    $("#" + goTo).css("display", "block");
 }
+
+// function editTask(event){
+//     event.stopPropogation();
+//     $(".form").display = block;
+//     $("#newTaskName").value = this.parentElement.getElementByClassName("taskName").textContent;
+//     $("#newTaskTime").value = this.parentElement.getElementByClassName("taskTime").textContent;
+//     $("#newTaskCat").value = this.parentElement.getElementByClassName("taskCat").textContent;
+//     taskID = this.parentElement.getElementByClassName("taskID").textContent;
+//     edit= true;
+
+// }
+
+// function newTask(){
+//     $(".form").display = block;
+//     edit = false;
+// }
+
+// function writeTasklist(){
+
+// }
+
+// function changePage(goTo){
+//     $(".tasklist").display = none;
+//     $(".schedule").display = none;
+//     $(goTo).display = block;
+// }
+
+// function makeTask(event){
+//     event.preventDefault();
+//     if(edit){
+//         tasks[taskID].name = $("#newTaskName").value;
+//         tasks[taskID].time = $("#newTaskTime").value;
+//         tasks[taskID].category = $("#newTaskCat").value;
+//         writeTasklist();
+//     }
+
+// }
