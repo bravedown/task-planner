969353bfc225966aa7c099027d715837f603cb68
David <davidbrown3142@gmail.com>
2020-09-19 10:56:04 -0400

    generateTimeblocks

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,4 +1,5 @@
 var info = JSON.parse(localStorage.getItem("task-planner-data"));
+var hours = [[12,"AM"],[1,"AM"],[2,"AM"],[3,"AM"],[4,"AM"],[5,"AM"],[6,"AM"],[7,"AM"],[8,"AM"],[9,"AM"],[10,"AM"],[11,"AM"],[12,"PM"],[1,"PM"],[2,"PM"],[3,"PM"],[4,"PM"],[5,"PM"],[6,"PM"],[7,"PM"],[8,"PM"],[9,"PM"],[10,"PM"],[11,"PM"]];
 !info ? info = {} : console.log("Loaded info from storage.");
 init();
 
@@ -42,4 +43,24 @@ function getTask() {
     info.getTaskState.state = false;
     storeInfo();
     changePage("schedule");
+}
+
+function generateTimeblocks(baseInterval = 60) {
+    $("#timeblocks").empty();
+    var timeblocks = [];
+    // Make and store each timeblock element in the timeblocks array
+    for (let i = 0; i < (24 * 60); i += baseInterval) timeblocks.push(makeTimeblock(i));
+    // Put them all on the page
+    timeblocks.forEach(e => $("#timeblocks").append(e));
+}
+
+function makeTimeblock(hour){
+    // Creates a string in the format of: hour = 270, hourStr = "4:30 AM", hour = 780, hourStr = "1 PM"
+    var hourArr = hours[Math.floor(hour / 60)];
+    var hourStr = hourArr[0] + (hour % 60 !== 0 ? `:${hour % 60}` : "") + ` ${hourArr[1]}`;
+    var newBlock = $(`<div id=${hour}>`);
+    var timeSpan = $(`<span class="hour">${hourStr}</span>`);
+    var taskSpan = $(`<span class="task">`);
+    newBlock.append(timeSpan, taskSpan);
+    return newBlock;
 }
