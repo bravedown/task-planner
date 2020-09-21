47da24d912d0927ed5d3701269b164ec0be86f9c
MichaelMayor <michael.davidge.mayor@gmail.com>
2020-09-21 19:27:13 -0400

    Moved Calendar to Script File

--- Assets/script.js
+++ Assets/script.js
@@ -34,3 +34,95 @@ $.ajax(settings).done(function (response) {
     document.getElementById("quoteText").innerHTML = response.text+"\"";
     document.getElementById("quoteAuthor").innerHTML = "-" + response.author;
 });
+
+var today = new Date();
+var currentMonth = today.getMonth();
+var currentYear = today.getFullYear();
+var selectYear = document.getElementById("year");
+var selectMonth = document.getElementById("month");
+
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+var monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+  if (currentMonth === 11) {
+    currentYear++;
+    currentMonth = 0;
+  } else {
+    currentMonth++;
+  }
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  if (currentMonth === 0) {
+    currentYear--;
+    currentMonth = 11;
+  } else {
+    currentMonth--;
+  }
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+  let firstDay = (new Date(year, month)).getDay();
+
+  tbl = document.getElementById("calendar-body"); // body of the calendar
+
+  // clearing all previous cells
+  tbl.innerHTML = "";
+
+  // filing data about month and in the page via DOM.
+  monthAndYear.innerHTML = months[month] + " " + year;
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  // creating all cells
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    // creates a table row
+    let row = document.createElement("tr");
+
+    //creating individual cells, filing them up with data.
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cellText = document.createTextNode(date);
+        if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+          cell.classList.add("is-info");
+        } // color today's date
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        date++;
+      }
+
+
+    }
+
+    tbl.appendChild(row); // appending each row into calendar body.
+  }
+
+}
+
+
+// check how many days in a month
+function daysInMonth(month, year) {
+  return 32 - new Date(year, month, 32).getDate();
+}
