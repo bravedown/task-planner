f8454ab5b51201e7f046e69019694eca04145685
David <davidbrown3142@gmail.com>
2020-09-23 15:04:50 -0400

    commit

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,24 +1,50 @@
 var info = JSON.parse(localStorage.getItem("task-planner-data"));
-var hours = [[12,"AM"],[1,"AM"],[2,"AM"],[3,"AM"],[4,"AM"],[5,"AM"],[6,"AM"],[7,"AM"],[8,"AM"],[9,"AM"],[10,"AM"],[11,"AM"],[12,"PM"],[1,"PM"],[2,"PM"],[3,"PM"],[4,"PM"],[5,"PM"],[6,"PM"],[7,"PM"],[8,"PM"],[9,"PM"],[10,"PM"],[11,"PM"]];
+var hours = [["12","AM"],["1","AM"],["2","AM"],["3","AM"],["4","AM"],["5","AM"],["6","AM"],["7","AM"],["8","AM"],["9","AM"],["10","AM"],["11","AM"],["12","PM"],["1","PM"],["2","PM"],["3","PM"],["4","PM"],["5","PM"],["6","PM"],["7","PM"],["8","PM"],["9","PM"],["10","PM"],["11","PM"]];
+var time;
 !info ? info = {
-    placeTaskState: {},
-    getTaskState: {},
+    placeTaskState: {
+        state: false
+    },
+    getTaskState: {
+        state: false
+    },
     taskCats: {}
 } 
 : console.log("Loaded info from storage.");
 init();
 
-function init() {
-    generateTimeblocks();
+function init() { 
+    setInterval(generateTimeblocks(baseInterval = 60), 60000)
     generateTasks();
-    $(document).on("click", ".timeblock", () => placeTaskState.state ? placeTask() : startGetTask() );
-    $(document).on("click", ".task", () => getTaskState.state ? getTask() : startPlaceTask() );
+    setDay();
+    $(document).on("click", ".timeblock", () => info.placeTaskState.state ? placeTask() : startGetTask() );
+    $(document).on("click", ".task", () => info.getTaskState.state ? getTask() : startPlaceTask() );
     $("#new-cat").on("click", makeTaskCategory);
+    $(document).on("click", ".navLink", navClick);
+}
+
+function navClick() {
+    let nav = $(this).attr("id");
+    let page = nav == "tasksNav" ? "taskView"
+        :nav == "calNav" ? "monthView"
+        :nav == "monthNav" ? "monthView"
+        :nav == "weekNav" ? "weekView"
+        :"dayView";
+    changePage(page);
+}
+
+function updateTime() {
+    time = moment();
+}
+
+function setDay() {
+    updateTime();
+    $("#focusedDay").text(time.format("dddd[,] MMMM Do") )
 }
 
 function startPlaceTask() {
     var taskData = $(this).attr("data-task");
-    changePage("schedule");
+    changePage("dayView");
     info.placeTaskState = {
         state: true,
         task: taskData
@@ -40,7 +66,7 @@ function startGetTask() {
         taskArea: `#${$(this).attr("id")}-task`
     };
     storeInfo();
-    changePage("schedule");
+    changePage("dayView");
 }
 
 // When you click a task to place it in a timeblock
@@ -48,7 +74,7 @@ function getTask() {
     $(info.getTaskState.taskArea).text($(this).attr("data-task"));
     info.getTaskState.state = false;
     storeInfo();
-    changePage("schedule");
+    changePage("dayView");
 }
 
 function generateTimeblocks(baseInterval = 60) {
@@ -57,24 +83,43 @@ function generateTimeblocks(baseInterval = 60) {
     // Make and store each timeblock element in the timeblocks array
     for (let i = 0; i < (24 * 60); i += baseInterval) timeblocks.push(makeTimeblock(i));
     // Put them all on the page
-    timeblocks.forEach(e => $("#timeblocks").append(e));
+    timeblocks.forEach(e => $("#dayView").append(e));
 }
 
 function makeTimeblock(hour){
-    // Creates a string in the format of: hour = 270, hourStr = "4:30 AM", hour = 780, hourStr = "1 PM"
     var hourArr = hours[Math.floor(hour / 60)];
-    var hourStr = hourArr[0] + (hour % 60 !== 0 ? `:${hour % 60}` : "") + ` ${hourArr[1]}`;
-    var newBlock = $(`<div id=${hour}>`);
-    var timeSpan = $(`<span class="hour">${hourStr}</span>`);
-    var taskSpan = $(`<span class="task">`);
-    newBlock.append(timeSpan, taskSpan);
+    var newBlock = $(`<div id="${hour}" class="columns block timeblock">`);
+    var timeCol = $(`<div class="column is-one-quarter">`);
+    timeCol.append($(`<h3 class="title" style="font-family: monospace;">${hourArr[0] + (hour % 60 !== 0 ? `:${hour % 60}` : ":00")}<span>${(hourArr[0].length === 1 ? "_" : "" )}</span>${` ${hourArr[1]}`}</h3>`));
+    var taskCol = $(`<div class="column">`);
+    taskCol.append(`<table id="hour${hour}Tasks" class="table is-striped">`)
+    newBlock.append(timeCol, taskCol);
     return newBlock;
+
+    // <div class="columns block" style="border-bottom-style: groove;">
+    //     <div class="column is-one-quarter">
+    //         <h1 class="title">0:00</h1>
+    //     </div>
+    //     <div class="column">
+    //         <table id="hour0Tasks" class="table is-striped">
+
+    //         </table>
+    //     </div>
+    // </div>
 }
 
 function changePage(goTo){
-    $("#tasklist").css("display", "none");
-    $("#schedule").css("display", "none");
-    $("#" + goTo).css("display", "block");
+    $("#taskView").addClass("is-hidden");
+    $("#dayView").addClass("is-hidden");
+    $("#monthView").addClass("is-hidden");
+    $("#weekView").addClass("is-hidden");
+    $("#" + goTo).removeClass("is-hidden");
+    $("#monthNav").removeClass("is-active");
+    $("#dayNav").removeClass("is-active");
+    $("#weekNav").removeClass("is-active");
+    $("#dayNav").addClass("is-active");
+    
+
 }
 
 function generateTasks() {
@@ -103,6 +148,7 @@ function makeTask() {
     generateTasks();
 }
 
+
 // function editTask(event){
 //     event.stopPropagation();
 //     $("#form").css("display", "block");
@@ -126,7 +172,7 @@ function makeTask() {
 //         tasks[taskID].name = $("#newTaskName").value;
 //         tasks[taskID].time = $("#newTaskTime").value;
 //         tasks[taskID].category = $("#newTaskCat").value;
-//         writeTasklist();
+//         writetaskView();
 //     }
 
 // }
